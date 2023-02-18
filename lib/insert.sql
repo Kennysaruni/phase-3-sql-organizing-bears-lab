b008a3983274a576e54758fc8517d7cca8b2be7e
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-02-18 17:02:10 +0300

    Completed Lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+NSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr. Chocolate", 35,"M","black","naive",1),
+("Rowdy", 25,"M","latina","Nice",1),
+("Tabitha", 29,"F","white","low",1 ),
+("Sergeant Brown", 45,"M","black","high",0),
+("Melissa", 19,"F","white","high",1),
+("Grinch", 25,"M","black","aggressive",0),
+("Wendy", 20,"M","black","Slimy",1),
+( null, 3, "F", "white", "goofy", 1 );
